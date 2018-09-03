23a2eb82ca80d1c962cb7ee50efc1ca0fcc605ae
Estudiante <Estudiante@10.203.1.83>
2018-09-03 08:09:10 -0500

    first commit

---
+++ src/unal/poo/practica/RobotBase6.java
@@ -0,0 +1,57 @@
+package unal.poo.practica;
+
+import becker.robots.*;
+
+/** 
+ * Practica de los conceptos de Programacion Estructurada
+ * @author Fabian Andres Giraldo */
+public class RobotBase6
+{    
+       //Declaracion de Variables -- Forma temporal - No es buena practica tener
+       //variables estaticas
+        public static City objetos;
+        public static Robot estudiante;
+        public static  boolean matriz [][] = new boolean[2][5];
+	public static void main (String[] args){
+            //Declarar la creacion de la ciudad
+            objetos = new City("Field6.txt");
+	    objetos.showThingCounts(true);
+            
+            //Direction.NORTH, EAST, SOUTH, WEST
+            //Definicion de la ubicacion del robot, Ciudad, posicion, Direccion, Numero things en el bolso.
+            estudiante = new Robot(objetos,6, 1, Direction.NORTH,0);
+           
+            
+          
+           
+           
+           
+            
+	}
+        
+        public static void creacionFuncion(int parametroEntrada){
+            for (int i = 0; i < parametroEntrada; i++) 
+                estudiante.move();
+        }
+
+       
+           
+           
+           
+       }
+
+
+
+
+
+
+
+
+
+
+}
+        
+            
+        
+
+
