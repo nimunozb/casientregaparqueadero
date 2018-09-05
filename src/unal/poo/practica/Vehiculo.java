3774c6885869974e955ddfce8b0dcdb8990e2c97
Horacio <Horacio@aguacristal>
2018-09-05 13:38:18 -0500

    park1

---
+++ src/unal/poo/practica/Vehiculo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unal.poo.practica;
+
+public class Vehiculo {
+    private String placa;
+    private Posicion posicion;
+
+    public Vehiculo(String placa, Posicion posicion) {
+        this.placa = placa;
+        this.posicion = posicion;
+    }
+
+    public Vehiculo(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+    
+    
+}
