507ab96832e16a97254699cd78c0af6ea26c1bd8
Horacio <Horacio@aguacristal>
2018-09-05 16:19:01 -0500

    park12

--- src/unal/poo/practica/RobotBase6.java
+++ src/unal/poo/practica/RobotBase6.java
@@ -11,7 +11,6 @@ public class RobotBase6
        //variables estaticas
         public static City objetos;
         public static Robot estudiante;
-        public static  boolean matriz [][] = new boolean[2][5];
 	public static void main (String[] args){
             //Declarar la creacion de la ciudad
             objetos = new City("Field6.txt");
@@ -19,26 +18,87 @@ public class RobotBase6
             
             //Direction.NORTH, EAST, SOUTH, WEST
             //Definicion de la ubicacion del robot, Ciudad, posicion, Direccion, Numero things en el bolso.
-            estudiante = new Robot(objetos,6, 1, Direction.NORTH,0);
+            estudiante = new Robot(objetos,7, 1, Direction.NORTH,0);
            
-            
-          
+        
+          recogerCarro(3);
+          llevarcarro();
+          dejarcarro(3);
            
            
            
             
 	}
+        public static void voltear(int grados){
+            switch(grados){
+                case 90:estudiante.turnLeft();break;
+                case 180:estudiante.turnLeft();estudiante.turnLeft();break;
+                case 270:estudiante.turnLeft();estudiante.turnLeft();estudiante.turnLeft();break;
+                default:System.out.println("agulo incorrecto"); ;break;
+            } }  
+           public static  void correr(int a){
+               for (int i = 0; i < a; i++) {
+                   estudiante.move();
+               }
+            
+        }  
         
-        public static void creacionFuncion(int parametroEntrada){
-            for (int i = 0; i < parametroEntrada; i++) 
-                estudiante.move();
-        }
+        public static void recogerCarro(int carrosadesplazar){
+            
+            correr(1);
+            voltear(270);
+            correr(1);
+            voltear(90);
+            correr(carrosadesplazar+1);
+            voltear(90);
+            correr(1);
+            voltear(90);
+            //pick athing
+            
+            correr(carrosadesplazar+2);}
+
+   public static void llevarcarro(){
+       int a= estudiante.getAvenue();
+       voltear(90);
+       correr(10-a);
+       voltear(90);
+       
+   }
+  public static void dejarcarro(int carrosadespazar){
+      correr(2);
+      voltear(270);
+      correr(carrosadespazar-1);
+      voltear(90);
+      correr(1);
+      voltear(90);
+      //
+      
+      correr(carrosadespazar-1);
+      voltear(90);
+      correr(3);
+      
+  }
+  
+  public static void normativa(int seccion,Vehiculo z){
+     Posicion x= z.getPosicion();
+     
+  }
+
+
+
+
+
+
+
+
+}
+       
 
        
            
            
            
-       }
+       
 
 
 
